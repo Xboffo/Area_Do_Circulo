33613f5921cda1e4e527883bcd9c6624989ad591
Kauet Lima <kauet123@live.com>
2022-12-11 16:58:45 -0300

    Primeiro Commit

---
+++ 1002/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double area, raio,n;
+            raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);    
+            n = 3.14159;
+            area = n * (Math.Pow (raio, 2));
+
+            Console.WriteLine("A=" + area.ToString("F4",CultureInfo.InvariantCulture));
+        }
+    }
+}
